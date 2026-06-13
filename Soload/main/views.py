98982a222d22f914038fd694db246e6bfe8032ab
yoonjay06 <yjaewon1020@gmail.com>
2026-06-14 02:46:44 +0900

    Feat: admin 계정까지 생성 완료. start페이지 링크 전부 붙여놨습니다

--- Soload/main/views.py
+++ Soload/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate, login as auth_login
 
 # Create your views here.
 def main(request):
@@ -9,6 +9,21 @@ def main(request):
     })
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            auth_login(request, user)
+
+            return redirect('main')
+
     return render(request, 'pages/login.html')
 
 def signup(request):
